c8baeec4aab31c9481abb4f5d5d35b10c2311c6d
CIITSHSDTP00\CIIT <student@ciit.edu.ph>
2026-01-28 09:07:27 +0800

    week 3 backend files

--- activities/backend/index.js
+++ activities/backend/index.js
@@ -1,34 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "../backEnd/config/db.js";
 
-const app = express();
-const PORT = 2999;
+    const app = express();
+    const PORT = 3000;
 
-app.use(express.json());
+      dotenv.config();
+      app.use(express.json());
+      app.use('/api/auth', authroutes)
 
-app.get("/getName", (req, res) => {
-  var name = "Ruiz";
-  res.status(200).json(name);
-});
-
-app.post("/login", (req,res) => {
-  var {username, password} = req.body;
-  if (username == "Ruiz" && password == "123pass") {
-    res.status(200).json({
-      message: "Login Succesfully",
-      status: "Success"
-    })
-  } else {
-    res.status(403).json({
-      message: "Invalid Username or Password",
-      status: "Failed"
-    }) 
-  }
-});
-
-app.listen(PORT, () => {
-  //get -> fetch, name var name="Ruiz"
-
-  //post -> logic; if username="Ruiz", password="123pass" alert login succesful
-
-  console.log(`Server is Running on PORT: ${PORT}`);
-});
+      app.listen(PORT, () => {
+        // GET -> fetch, name var name = "name"
+        // POST -> logic, if username = "name", password = "123" alert login success
+        connectDB();
+        console.log(`Server is Running on PORT: ${PORT}`);
+      });
